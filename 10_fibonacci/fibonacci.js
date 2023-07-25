904f9d5d5457b8972f1bdf885fed83466dc0ab46
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-07-25 11:07:46 +0200

    Solve ecercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [0, 1];
+    if(n < 0) return "OOPS";
+    for (let i = 2; i < 26; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return arr[parseInt(n)];
 };
 
 // Do not edit below this line
